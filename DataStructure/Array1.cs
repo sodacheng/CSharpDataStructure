a4f306e2d3fddef54c46f88e9cb5fa77966b2c70
橙飒 <634377685@qq.com>
2022-06-27 13:41:32 +0800

    2-1 是什么是动态数组

---
+++ DataStructure/Array1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Array1
+    {
+        private int[] data; // 静态数组进行元素的存储
+        private int N; //记录动态数组实际存储了多少元素
+
+        /// <summary>
+        /// 用户一开始指定动态数组的容量
+        /// </summary>
+        /// <param name="capacity"></param>
+        public Array1(int capacity)
+        {
+            data = new int[capacity];
+            N = 0;
+        }
+
+        /// <summary>
+        /// 用户一开始没有指定动态数组容量
+        /// </summary>
+        public Array1() : this(10) { }
+
+        public int Capacity => data.Length; // 返回数组的容量
+
+        public int Count => N; // 返回数组实际存储元素的多少
+
+        public bool IsEmpty => N == 0; // 查看数组是否为空
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10]; // 创建了一个int类型的数组 容量为10
+            for(int i = 0; i < 10; i++)
+            {
+                arr[i] = i; 
+                Console.WriteLine(arr[i]+" ");
+            }
+
+            // 使用动态数组 ArrayList
+            ArrayList a = new ArrayList(10);// 可以传入一个参数来指定动态数组容量的大下, 不传的话具备一个默认的容量
+            for(int i = 0; i < 15; i++) // 动态数具备自动扩容的能力
+            {
+                a.Add(i);
+                Console.WriteLine(a[i]+" ");
+            }
+
+            // List<T>
+            List<int> l = new List<int>(10);
+            for(int i = 0; i<15;i++)
+            {
+                l.Add(i);
+                Console.WriteLine(l[i]);
+            }    
+        }
+    }
+}
